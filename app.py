ac0a0f18d6aba0a414592030ca143ab6dcbd1968
Yves Tanas <Yves.Tanas@gmx.de>
2026-03-04 16:45:12 +0100

    Setup

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
- 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/library.sqlite"
+db = SQLAlchemy(app)
